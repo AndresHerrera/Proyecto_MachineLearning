37857a95403b93cc026e0dc5987f17a3f2de1cd7
Andres Herrera <t763rm3n@gmail.com>
2017-12-13 08:29:24 -0500

    ML

---
+++ up.bat
@@ -0,0 +1,6 @@
+rem echo "# Proyecto_PatternRecognition" >> README.md
+rem git init
+git add .
+git commit -m "ML"
+rem git remote add origin https://github.com/AndresHerrera/Proyecto_MachineLearning.git
+git push -u origin master
